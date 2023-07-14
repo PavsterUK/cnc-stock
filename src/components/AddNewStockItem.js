6be6e2d0aedd0b1dd5ada95b32f6c068fb8b39ce
Pavel Naumovich <pavelnaumovic@gmail.com>
2023-07-14 21:56:19 +0100

    grid add new item

--- src/components/AddNewStockItem.js
+++ src/components/AddNewStockItem.js
@@ -1,21 +1,17 @@
 import * as React from "react";
 import Modal from "@mui/material/Modal";
 import {
-  Box,
   Typography,
-  Table,
-  TableHead,
-  TableBody,
-  TableRow,
-  TableCell,
   Button,
   TextField,
   MenuItem,
   Select,
   InputLabel,
+  Grid,
 } from "@mui/material/";
 import FormControl from "@mui/material/FormControl";
 import AlertDialog from "./AlertDialog";
+import styles from "./AddNewStockItem.module.css";
 
 const style = {
   position: "absolute",
@@ -27,10 +23,6 @@ const style = {
   border: "2px solid #000",
   boxShadow: 24,
   p: 4,
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  flexDirection: "column",
 };
 
 const categories = [
@@ -48,8 +40,8 @@ const categories = [
   },
 ];
 
-export default function AddNewStockItem({ AddNewStockItemOpenOpen }) {
-  const [open, setOpen] = React.useState(AddNewStockItemOpenOpen);
+export default function AddNewStockItem() {
+  const [open, setOpen] = React.useState();
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [selectedAttributes, setSelectedAttributes] = React.useState([]);
@@ -73,183 +65,152 @@ export default function AddNewStockItem({ AddNewStockItemOpenOpen }) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography
-            id="modal-modal-title"
-            variant="h6"
-            component="h2"
-            align="center"
-          ></Typography>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell colSpan={8}>
-                  <Typography
-                    variant="h6"
-                    component="h2"
-                    align="center"
-                    style={{ fontWeight: "bold" }}
-                  >
-                    Add a New Item To Stock
-                  </Typography>
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Box
-                    component="form"
-                    sx={{
-                      "& .MuiTextField-root": { m: 1, width: "100%" },
-                    }}
-                    noValidate
-                    autoComplete="off"
-                  >
-                    <TextField
-                      fullWidth
-                      required
-                      id="outlined-required"
-                      label="Item Code or Title"
-                    />
-                    <TextField
-                      fullWidth
-                      required
-                      id="outlined-required"
-                      label="Item description"
-                    />
-                    <TextField
-                      required
-                      id="outlined-required"
-                      label="Item Location"
-                      sx={{ maxWidth: "20ch" }}
-                    />
-                    <TextField
-                      id="outlined-required"
-                      label="Brand"
-                      sx={{ maxWidth: "20ch" }}
-                    />
-                    <div>
-                      <TextField
-                        id="outlined-select-currency"
-                        select
-                        label="Choose item Category"
-                      >
-                        {categories.map((category, index) => (
-                          <MenuItem key={index} value={category.value}>
-                            {category.value}
-                          </MenuItem>
-                        ))}
-                      </TextField>
-                      <FormControl fullWidth>
-                        <InputLabel id="item-attributes-label">
-                          Suitable Materials
-                        </InputLabel>
-                        <Select
-                          label="Suitable Materials"
-                          labelId="item-attributes-label"
-                          id="item-attributes-select"
-                          multiple
-                          value={selectedAttributes}
-                          onChange={handleAttributeChange}
-                        >
-                          <MenuItem
-                            value="P"
-                            sx={{
-                              backgroundColor: selectedAttributes.includes("P")
-                                ? "#0eb6f7 !important"
-                                : "transparent",
-                              color: selectedAttributes.includes("P")
-                                ? "white !important"
-                                : "black",
-                            }}
-                          >
-                            Mild Steel
-                          </MenuItem>
-                          <MenuItem
-                            value="M"
-                            sx={{
-                              backgroundColor: selectedAttributes.includes("M")
-                                ? "#f6ea02 !important"
-                                : "transparent",
-                            }}
-                          >
-                            Stainless
-                          </MenuItem>
-                          <MenuItem
-                            value="K"
-                            sx={{
-                              backgroundColor: selectedAttributes.includes("K")
-                                ? "#e31c1e !important"
-                                : "transparent",
-                              color: selectedAttributes.includes("K")
-                                ? "white !important"
-                                : "black",
-                            }}
-                          >
-                            Cast Iron
-                          </MenuItem>
-                          <MenuItem
-                            value="N"
-                            sx={{
-                              backgroundColor: selectedAttributes.includes("N")
-                                ? "#2dc65b !important"
-                                : "transparent",
-                              color: selectedAttributes.includes("N")
-                                ? "white !important"
-                                : "black",
-                            }}
-                          >
-                            Non Ferrous
-                          </MenuItem>
-                          <MenuItem
-                            value="S"
-                            sx={{
-                              backgroundColor: selectedAttributes.includes("S")
-                                ? "#f77b00 !important"
-                                : "transparent",
-                              color: selectedAttributes.includes("S")
-                                ? "white !important"
-                                : "black",
-                            }}
-                          >
-                            Super Alloy
-                          </MenuItem>
-                          <MenuItem
-                            value="H"
-                            sx={{
-                              backgroundColor: selectedAttributes.includes("H")
-                                ? "#bababa !important"
-                                : "transparent",
-                              color: selectedAttributes.includes("H")
-                                ? "white !important"
-                                : "black",
-                            }}
-                          >
-                            Hardened Steel
-                          </MenuItem>
-                        </Select>
-                      </FormControl>
-                    </div>
-                  </Box>
-                </TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>
-                  <AlertDialog
-                    onClick={handleSendRequest}
-                    name={"Add Item"}
-                    handleCloseParent={handleClose}
-                    dialogTitle={"New Item Added!"}
-                    dialogMessage={
-                      "Added Item will now appear on the stock list. Thank you!"
-                    }
-                  />
-                </TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </Box>
+        <Grid container spacing={2} sx={style}>
+          <Grid item xs={12} md={12} lg={12}>
+            <Typography
+              id="modal-modal-title"
+              variant="h4"
+              component="h2"
+              align="center"
+            >
+              Add New Item
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} md={8} lg={8}>
+            <TextField
+              fullWidth
+              required
+              id="outlined-required"
+              label="Item Code or Title"
+            />
+          </Grid>
+          <Grid item xs={12} md={4} lg={4}>
+            <TextField
+              fullWidth
+              id="outlined-required"
+              label="Brand"
+            />
+          </Grid>
+
+          <TextField fullWidth required id="outlined-required" label="Item description" />
+
+          <TextField fullWidth required id="outlined-required" label="Item Location" />
+          <TextField fullWidth required id="outlined-required" label="Supplier" />
+
+          <TextField
+            fullWidth
+            id="outlined-select-currency"
+            select
+            label="Choose item Category"
+          >
+            {categories.map((category, index) => (
+              <MenuItem key={index} value={category.value}>
+                {category.value}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <FormControl classes={{ root: styles.materials }}>
+            <InputLabel id="item-attributes-label">
+              Suitable Materials
+            </InputLabel>
+            <Select
+              label="Suitable Materials"
+              labelId="item-attributes-label"
+              id="item-attributes-select"
+              multiple
+              value={selectedAttributes}
+              onChange={handleAttributeChange}
+            >
+              <MenuItem
+                value="P"
+                sx={{
+                  backgroundColor: selectedAttributes.includes("P")
+                    ? "#0eb6f7 !important"
+                    : "transparent",
+                  color: selectedAttributes.includes("P")
+                    ? "white !important"
+                    : "black",
+                }}
+              >
+                Mild Steel
+              </MenuItem>
+              <MenuItem
+                value="M"
+                sx={{
+                  backgroundColor: selectedAttributes.includes("M")
+                    ? "#f6ea02 !important"
+                    : "transparent",
+                }}
+              >
+                Stainless
+              </MenuItem>
+              <MenuItem
+                value="K"
+                sx={{
+                  backgroundColor: selectedAttributes.includes("K")
+                    ? "#e31c1e !important"
+                    : "transparent",
+                  color: selectedAttributes.includes("K")
+                    ? "white !important"
+                    : "black",
+                }}
+              >
+                Cast Iron
+              </MenuItem>
+              <MenuItem
+                value="N"
+                sx={{
+                  backgroundColor: selectedAttributes.includes("N")
+                    ? "#2dc65b !important"
+                    : "transparent",
+                  color: selectedAttributes.includes("N")
+                    ? "white !important"
+                    : "black",
+                }}
+              >
+                Non Ferrous
+              </MenuItem>
+              <MenuItem
+                value="S"
+                sx={{
+                  backgroundColor: selectedAttributes.includes("S")
+                    ? "#f77b00 !important"
+                    : "transparent",
+                  color: selectedAttributes.includes("S")
+                    ? "white !important"
+                    : "black",
+                }}
+              >
+                Super Alloy
+              </MenuItem>
+              <MenuItem
+                value="H"
+                sx={{
+                  backgroundColor: selectedAttributes.includes("H")
+                    ? "#bababa !important"
+                    : "transparent",
+                  color: selectedAttributes.includes("H")
+                    ? "white !important"
+                    : "black",
+                }}
+              >
+                Hardened Steel
+              </MenuItem>
+            </Select>
+          </FormControl>
+
+          <AlertDialog
+            onClick={handleSendRequest}
+            name={"Add Item"}
+            handleCloseParent={handleClose}
+            dialogTitle={"New Item Added!"}
+            dialogMessage={
+              "Added Item will now appear on the stock list. Thank you!"
+            }
+          />
+        </Grid>
       </Modal>
     </>
   );
